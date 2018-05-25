813be8c62835bc6528d45a118eb6089ef773462f
luispaulocaringal <38064859+luispaulocaringal@users.noreply.github.com>
2018-05-26 00:24:21 +0800

    removed overlapping schedules

--- Project/function/insert.php
+++ Project/function/insert.php
@@ -8,20 +8,32 @@
 		$connect = new PDO('mysql:host=localhost;dbname=itelec','root','root');
 		
 		$endTime = date("H:i:s",strtotime("+".$duration." minutes",strtotime($startTime)));
-		
-		$query = "INSERT INTO schedule(profName,section,subject,roomNumber,startTime,endTime,date,day,duration,rfr,status) VALUES ('".$profName."','".$section."','".$subject."',".$roomNumber.",'".$startTime."','".$endTime."','".$date."','".$day."',".$duration.",'".$rfr."','pending')";
-		$stmt = $connect->prepare($query);
-
-
-		$stmt->execute();
-
-		logAction("request");
-		
-		session_destroy();
-		
-		echo '<script type="text/javascript">'; 
-		echo 'alert("Request sent!");'; 
-		echo 'window.location.href = "index.php"';
-		echo '</script>';
+			
+		$class = loadClass($roomNumber,$day);
+		//$pending = loadPending($roomNumber,$date);
+		//$reserved = loadReserved($roomNumber,$date);
+		$boolean = false;
+		foreach($class as $val){
+			if(strtotime($endTime)>strtotime($val["startTime"])&&strtotime($endTime)<=strtotime($val["endTime"])){
+				echo "hello";
+				$boolean = true;		
+				echo '<script type="text/javascript">'; 
+				echo 'alert("Classes are scheduled during those periods!");'; 
+				echo 'window.location.href = "schedule.php"';
+				echo '</script>';
+				break;
+			}
+		}
+		if($boolean == false){		
+			$query = "INSERT INTO schedule(profName,section,subject,roomNumber,startTime,endTime,date,day,duration,rfr,status) VALUES ('".$profName."','".$section."','".$subject."',".$roomNumber.",'".$startTime."','".$endTime."','".$date."','".$day."',".$duration.",'".$rfr."','pending')";
+			$stmt = $connect->prepare($query);
+			$stmt->execute();
+			logAction("request");		
+			session_destroy();			
+			echo '<script type="text/javascript">'; 
+			echo 'alert("Request sent!");'; 
+			echo 'window.location.href = "index.php"';
+			echo '</script>';
+		}
 	}
 ?>
